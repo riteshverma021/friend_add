7e7a13097c819503bc864237124828f933662564
riteshverma021 <riteshverma2338@gmail.com>
2025-02-07 20:50:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,10 +38,10 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
  
   if(process.env.NODE_ENV ==="production")
 {
-    app.use(express.static(path.join(__dirname ,"../frontend/dist")))
+    app.use(express.static(path.join(__dirname ,"../../frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend" , "dist" , "index.html"))
+    res.sendFile(path.join(__dirname,"../../frontend" , "dist" , "index.html"))
 })
 
 }
